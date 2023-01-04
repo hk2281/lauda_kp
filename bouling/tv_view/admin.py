10d31edd456f0b87ec710cf3a3249b9d6da5d79c
hk2281 <strapkobrest@gmail.com>
2023-01-04 14:19:12 +0000

    ending model, testing in admin panel

--- bouling/tv_view/admin.py
+++ bouling/tv_view/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
-
+from .models import Players, Command, Ligue, Game, ClubMeta
 # Register your models here.
+
+
+class a_players(admin.ModelAdmin):
+    list_display = [
+        'player',
+        'comand_id',
+        'first_name',
+        'last_name',
+        'contact_info',
+        'way',
+        'throw_count',
+        'avarege_score',
+    ]
+
+class a_command(admin.ModelAdmin):
+    list_display = [
+        'command',
+        'comand_name',
+        'score',
+    ]
+
+class a_club(admin.ModelAdmin):
+    list_display = [
+        'club',
+        'address',
+        'way_count',
+    ]
+
+class a_ligue(admin.ModelAdmin):
+    list_display = [
+        'ligue',
+        'ligue_name',
+        'club_id',
+    ]
+class a_game(admin.ModelAdmin):
+    list_display = [
+        'game',
+        'game_name',
+        'game_date',
+    ]
+
+admin.site.register(Players,a_players)
+admin.site.register(Command,a_command)
+admin.site.register(Game,a_game)
+admin.site.register(Ligue,a_ligue)
+admin.site.register(ClubMeta,a_club)
--- bouling/tv_view/models.py
+++ bouling/tv_view/models.py
@@ -1,3 +1,50 @@
+
+
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class ClubMeta(models.Model):
+
+    club = models.AutoField(primary_key=True)
+    club_name = models.CharField(max_length=30, default=None, unique=True)
+    address = models.CharField(max_length=30)
+    way_count = models.IntegerField()
+
+
+class Ligue(models.Model):
+    ligue = models.AutoField(primary_key=True)
+    ligue_name = models.CharField(max_length=30)
+    club_id = models.ForeignKey(ClubMeta, on_delete=models.CASCADE)
+
+
+class Game(models.Model):
+    game = models.AutoField(primary_key=True)
+    game_name = models.CharField(max_length=30)
+    game_date = models.DateTimeField()
+    
+
+class Command(models.Model):
+    command = models.AutoField(primary_key=True)
+    game_id = models.ForeignKey(Game, on_delete=models.CASCADE, default=None)
+    lugue_id = models.ForeignKey(Ligue, on_delete=models.CASCADE, default=None)
+    comand_name = models.CharField(max_length=50)
+    score = models.FloatField()
+    
+
+
+class Players(models.Model):
+    """this class describe player entity model"""
+    player = models.AutoField(primary_key=True)
+    comand_id = models.ForeignKey(Command, on_delete = models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    contact_info = models.CharField(max_length=30)
+    way = models.IntegerField()
+    throw_count = models.IntegerField()
+    avarege_score = models.FloatField()
+
+
+
+
