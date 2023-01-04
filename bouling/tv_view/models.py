aa6f9ed709cc84412949180ca72555098cdad14b
hk2281 <strapkobrest@gmail.com>
2023-01-05 02:52:05 +0300

    Update models.py
    add validator inport adn constraint for int field, add non required fields

--- bouling/tv_view/models.py
+++ bouling/tv_view/models.py
@@ -2,23 +2,24 @@
 
 from django.db import models
 from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 class ClubMeta(models.Model):
 
     club = models.AutoField(primary_key=True)
-    club_name = models.CharField(max_length=30, default=None, unique=True)
-    address = models.CharField(max_length=30)
-    way_count = models.IntegerField()
+    club_name = models.CharField(max_length=30, default=None, unique=True,blank=False)
+    address = models.CharField(max_length=30, blank=False)
+    way_count = models.IntegerField(blank=True)
 
     def __str__(self) -> str:
         return self.club_name
 
 class Ligue(models.Model):
     ligue = models.AutoField(primary_key=True)
-    ligue_name = models.CharField(max_length=30)
-    club_id = models.ForeignKey(ClubMeta, on_delete=models.CASCADE)
+    ligue_name = models.CharField(max_length=30, blank=False)
+    club_id = models.ForeignKey(ClubMeta, on_delete=models.CASCADE, default=None)
 
     def __str__(self) -> str:
         return self.ligue_name
@@ -26,8 +27,8 @@ class Ligue(models.Model):
 
 class Game(models.Model):
     game = models.AutoField(primary_key=True)
-    game_name = models.CharField(max_length=30)
-    game_date = models.DateTimeField()
+    game_name = models.CharField(max_length=30,blank=False)
+    game_date = models.DateTimeField(blank=True)
 
     def __str__(self) -> str:
         return self.game_name
@@ -37,8 +38,8 @@ class Command(models.Model):
     command = models.AutoField(primary_key=True)
     game_id = models.ForeignKey(Game, on_delete=models.CASCADE, default=None)
     lugue_id = models.ForeignKey(Ligue, on_delete=models.CASCADE, default=None)
-    comand_name = models.CharField(max_length=50)
-    score = models.FloatField()
+    comand_name = models.CharField(max_length=50, blank=False)
+    score = models.FloatField(blank=True)
 
     def __str__(self) -> str:
         return self.comand_name
@@ -48,12 +49,14 @@ class Command(models.Model):
 class Players(models.Model):
     """this class describe player entity model"""
     player = models.AutoField(primary_key=True)
-    comand_id = models.ForeignKey(Command, on_delete = models.CASCADE)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    contact_info = models.CharField(max_length=30)
-    way = models.IntegerField()
-    throw_count = models.IntegerField()
+    comand_id = models.ForeignKey(Command, on_delete = models.CASCADE, default=None)
+    first_name = models.CharField(max_length=30,blank=False)
+    last_name = models.CharField(max_length=30,blank=False)
+    contact_info = models.CharField(max_length=30,blank=True)
+    way = models.IntegerField(blank=True,validators=[MinValueValidator(0),
+                                  MaxValueValidator(100)])
+    throw_count = models.IntegerField(blank=True, validators=[MinValueValidator(0),
+                                  MaxValueValidator(100)])
     avarege_score = models.FloatField()
 
     def __str__(self) -> str:
