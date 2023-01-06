ef82435dcc6c4b44fcb27e44a2b7b42dba6d13fe
hk2281 <strapkobrest@gmail.com>
2023-01-06 15:01:40 +0000

    return enter in liet

--- bouling/tv_view/admin.py
+++ bouling/tv_view/admin.py
@@ -5,42 +5,18 @@ from .models import Players, Command, Ligue, Game, ClubMeta
 
 class a_players(admin.ModelAdmin):
     list_display = [
-        'player',
-        'comand_id',
-        'first_name',
-        'last_name',
-        'contact_info',
-        'way',
-        'throw_count',
-        'avarege_score',
-    ]
+        'player','comand_id','first_name','last_name','contact_info','way','throw_count','avarege_score',]
 
 class a_command(admin.ModelAdmin):
-    list_display = [
-        'command',
-        'comand_name',
-        'score',
-    ]
+    list_display = ['command','comand_name','score',]
 
 class a_club(admin.ModelAdmin):
-    list_display = [
-        'club',
-        'address',
-        'way_count',
-    ]
+    list_display = ['club','club_name','address','way_count',]
 
 class a_ligue(admin.ModelAdmin):
-    list_display = [
-        'ligue',
-        'ligue_name',
-        'club_id',
-    ]
+    list_display = ['ligue','ligue_name','club_id',]
 class a_game(admin.ModelAdmin):
-    list_display = [
-        'game',
-        'game_name',
-        'game_date',
-    ]
+    list_display = ['game','game_name','game_date',]
 
 admin.site.register(Players,a_players)
 admin.site.register(Command,a_command)
